a7164f573d3135e4babac80eb3caeabb511ba100
Wagram Airiian <vagram.airiian@tum.de>
2019-04-10 14:53:04 +0200

    Users schema/routes/controller

---
+++ user/userRoutes.js
@@ -0,0 +1,14 @@
+module.exports = userRoutes;
+
+function userRoutes(passport) {
+
+    var userController = require('./userController');
+    var router = require('express').Router();
+
+    //router.post('/');
+    router.post('/login', userController.login);
+    router.post('/signup', userController.signup);
+    router.post('/deregister', passport.authenticate('jwt', {session: false}), userController.deregister);
+
+    return router;
+}
---
+++ user/userSchema.js
@@ -0,0 +1,61 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+var options = {discriminatorKey: 'kind'};
+
+var userSchema = mongoose.Schema({
+    firstname: {
+        type: String,
+        // required: true,
+        unique: false
+    },
+    lastname: {
+        type: String,
+        // required: true,
+        unique: false
+    },
+    birthday: {
+        type: Date,
+        // required: true,
+        unique: false
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, options);
+
+userSchema.pre('save', function(next) {
+    var user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    bcrypt.genSalt(10, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, null, function (err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
+
+module.exports = mongoose.model('users', userSchema);
