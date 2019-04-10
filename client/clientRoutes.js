6ec872cb4733af3cc9ccb8a8470c04460ea9bd1e
Wagram Airiian <vagram.airiian@tum.de>
2019-04-10 14:52:25 +0200

    Client schema/routes/controller

---
+++ client/clientRoutes.js
@@ -0,0 +1,19 @@
+module.exports = clientRoutes;
+
+function clientRoutes(passport) {
+
+    var clientController = require('./clientController');
+    var router = require('express').Router();
+    var unless = require('express-unless');
+
+    var mw = passport.authenticate('jwt', {session: false});
+    mw.unless = unless;
+
+    //middleware
+    router.use(mw.unless({method: ['GET']}));
+
+    //router.post('/');
+
+
+    return router;
+}
