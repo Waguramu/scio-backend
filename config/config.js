27563c788fcbd690698d8d2886d8120216789361
Vinpasso <vincent@jd-bode.de>
2019-04-16 13:20:28 +0200

    Added possibility to set db connection string from environment variable

--- config/config.js
+++ config/config.js
@@ -3,7 +3,13 @@ Config.db = {};
 Config.app={};
 Config.auth = {};
 
-Config.db.url = 'mongodb+srv://deeplegal:CRFtI1BAtouDXn9O@cluster0-i3jck.gcp.mongodb.net/test?retryWrites=true';
+if(process.env.DB_URL) {
+    Config.db.url = process.env.DB_URL;
+    console.log('Loaded DB URL from environment variable: ' + Config.db.url);
+} else {
+    Config.db.url = 'mongodb+srv://deeplegal:CRFtI1BAtouDXn9O@cluster0-i3jck.gcp.mongodb.net/test?retryWrites=true';
+    console.log('Using default DB URL: ' + Config.db.url);
+}
 
 // Use environment defined port or 3000
 Config.app.port = process.env.PORT || 3000;
